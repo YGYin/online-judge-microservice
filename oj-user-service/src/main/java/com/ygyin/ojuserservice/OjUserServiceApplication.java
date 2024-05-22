5fc3cda218637d5fe18c01c94be1ac08da5864e8
YGYin <ygyin0307@gmail.com>
2024-05-22 17:11:44 +0800

    User service transplantation

--- oj-user-service/src/main/java/com/ygyin/ojuserservice/OjUserServiceApplication.java
+++ oj-user-service/src/main/java/com/ygyin/ojuserservice/OjUserServiceApplication.java
@@ -1,9 +1,17 @@
 package com.ygyin.ojuserservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@MapperScan("com.ygyin.ojuserservice.mapper")
+@EnableScheduling
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@ComponentScan("com.ygyin")
 public class OjUserServiceApplication {
 
     public static void main(String[] args) {
---
+++ oj-user-service/src/main/java/com/ygyin/ojuserservice/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.ygyin.ojuserservice.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ygyin.ojmodel.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
