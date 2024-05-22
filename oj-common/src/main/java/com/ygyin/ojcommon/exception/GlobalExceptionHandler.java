746d283102ae51102bd6b2e9fc6edc7f63938d7a
YGYin <ygyin0307@gmail.com>
2024-05-22 15:45:17 +0800

    Common service transplantation

---
+++ oj-common/src/main/java/com/ygyin/ojcommon/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.ygyin.ojcommon.exception;
+
+import com.ygyin.ojcommon.common.BaseResponse;
+import com.ygyin.ojcommon.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import com.ygyin.ojcommon.common.ErrorCode;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ *
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
